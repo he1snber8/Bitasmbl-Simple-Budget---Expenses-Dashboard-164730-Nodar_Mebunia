104092b8c78fb4891dbc82dd30b29d825b01a1ee
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-22 13:12:52 +0400

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header>Simple Budget & Expenses Dashboard</header>
+      <main>
+        <section id="summary" />
+        <section id="forms" />
+        <section id="charts" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
