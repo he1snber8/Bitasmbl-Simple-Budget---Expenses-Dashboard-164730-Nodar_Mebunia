46dff801a59de87e40022c154d46760a923d3989
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-22 13:12:53 +0400

    Add store.js

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import budgetReducer from './budgetSlice';
+
+const store = configureStore({
+  reducer: { budget: budgetReducer }
+});
+
+export default store;
